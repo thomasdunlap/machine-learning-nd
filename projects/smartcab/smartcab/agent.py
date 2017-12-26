2bbc30f74969689d67f49f5cf8d89d9bcda19b8d
thomasdunlap <tom.a.dunlap@gmail.com>
2017-12-26 18:27:02 -0500

    feat: Completed learn().

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -154,6 +154,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
+        if self.learning:
+            self.Q[state][action] = (1.0 - self.alpha) * self.Q[state][action] + self.alpha * reward
 
         return
 
