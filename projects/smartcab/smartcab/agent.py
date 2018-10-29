52e7611bc8e6a657718438718e94b5a868af4c87
thomasdunlap <tom.a.dunlap@gmail.com>
2018-10-28 23:56:59 -0400

    feat: set tolerance to 0.001 in sim.run() in agent.py.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -218,7 +218,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=10, tolerance=0.001)
 
 
 if __name__ == '__main__':
